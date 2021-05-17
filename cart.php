10aa3ab6d9e46f865a67d88cbb0b9d4380bb2c04
Dimitris Kritsotakis <dimkri97@gmail.com>
2021-05-17 22:19:41 +0300

    Do not display table if cart is empty

--- cart.php
+++ cart.php
@@ -15,12 +15,20 @@ require_once 'includes/checkIfAdmin.php' ?>
         integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0"
         crossorigin="anonymous">
 
-  <title>Hello, world!</title>
+  <title>Cart</title>
 </head>
 <body>
 <?php require_once 'includes/navigation.php' ?>
 <h2>Your cart</h2>
 <p><h4>Here you can see your cart and proceed to payment when you're done</h4></p>
+<?php
+$cart = $_SESSION['cart'];
+
+
+if (count($cart) > 0) {
+
+
+?>
 <table class="table table-hover">
   <thead>
   <tr>
@@ -33,30 +41,37 @@ require_once 'includes/checkIfAdmin.php' ?>
   </thead>
   <tbody>
   <?php
-//  @todo $cart = $_SESSION['cart'];
-  $cart = [1,7,4,12];
 
-  foreach ($cart as $product_id) {
+    foreach ($cart as $product_id) {
 
-    $prod_categs_query = "select name, price, category from products where products.product_id=" .$product_id;
-    $product = $mysql->query($prod_categs_query)->fetch_assoc();
+      $prod_categs_query = "select name, price, category from products where products.product_id=" . $product_id;
+      $product = $mysql->query($prod_categs_query)->fetch_assoc();
 
 
-    $prod_categs_query = "select pt_category_name from pt_category where pt_category.pt_category_id = " . $product['category'];
-    $prod_category = $mysql->query($prod_categs_query)->fetch_array();
+      $prod_categs_query = "select pt_category_name from pt_category where pt_category.pt_category_id = " . $product['category'];
+      $prod_category = $mysql->query($prod_categs_query)->fetch_array();
 
-    echo "<tr>";
-    echo "<th scope='row'>$product_id</th>";
-    echo "<td>" . $product['name'] . "</td>";
-    echo "<td>" . $product['price'] . "€</td>";
-    echo "<td>" . $prod_category[0] ."</td>";
-    echo "<td>1</td>";
-    echo "</tr>";
-  }
+      echo "<tr>";
+      echo "<th scope='row'>$product_id</th>";
+      echo "<td>" . $product['name'] . "</td>";
+      echo "<td>" . $product['price'] . "€</td>";
+      echo "<td>" . $prod_category[0] . "</td>";
+      echo "<td>1</td>";
+      echo "</tr>";
+    }
 
   ?>
   </tbody>
 </table>
+<?php
+
+}
+else {
+  echo '<div class="alert alert-danger">You have not added any products to your cart!</div>';
+
+}
+
+?>
 
 <!-- @todo button proceed to payment -->
 
